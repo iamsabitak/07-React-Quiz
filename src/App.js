8252271b93ca7c63b058978b3bcb0b7a1a566dd3
sabita <sabitakhadka091@gmail.com>
2023-11-05 13:37:49 +0545

    Handling Loading, Error, and Ready Status and implemented to app component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,50 @@
-import React from "react";
+import React, { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+      throw new Error("Action Error");
+  }
+}
+const initialState = {
+  questions: [],
+  //'loading','ready','error','active','finished'
+  status: "loading",
+};
 function App() {
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const numQuestions = questions.length;
+  useEffect(function () {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
   return (
     <div className="app">
       <h1>
         <Header />
       </h1>
       <Main>
-        <p>1/15</p>
-        <p>Question?</p>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen numQuestions={numQuestions}/>}
       </Main>
     </div>
   );
