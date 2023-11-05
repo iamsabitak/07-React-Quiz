b6a60d1e877345809e92a65ff0224027dd663521
sabita <sabitakhadka091@gmail.com>
2023-11-05 15:55:53 +0545

    added a new component nextButton

---
+++ src/components/NextButton.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function NextButton({ dispatch, answer }) {
+  if (answer === null) return null;
+  return (
+    <button
+      className="btn btn-ui"
+      onClick={() => dispatch({ type: "nextQuestion" ,})}
+    >
+      Next
+    </button>
+  );
+}
