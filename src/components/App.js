7600aa674d826004e703989e774e855b2bba6557
sabita <sabitakhadka091@gmail.com>
2023-11-05 15:06:11 +0545

    added some button and implemented some feature and handle them

--- src/components/App.js
+++ src/components/App.js
@@ -24,7 +24,11 @@ function reducer(state, action) {
         ...state,
         status: "active",
       };
-
+    case "newAnswer":
+      return {
+        ...state,
+        answer: action.payload,
+      };
     default:
       throw new Error("Action Error");
   }
@@ -35,9 +39,10 @@ const initialState = {
   //'loading','ready','error','active','finished'
   status: "loading",
   index: 0,
+  answer: null,
 };
 function App() {
-  const [{ questions, status, index }, dispatch] = useReducer(
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
     reducer,
     initialState
   );
@@ -59,7 +64,13 @@ function App() {
         {status === "ready" && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === "active" && <Question question={questions[index]} />}
+        {status === "active" && (
+          <Question
+          question={questions[index]}
+          dispatch={dispatch}
+          answer={answer}
+          />
+        )}
       </Main>
     </div>
   );
--- src/components/Options.js
+++ src/components/Options.js
@@ -1,10 +1,16 @@
 import React from "react";
 
-export default function Options({question}) {
+export default function Options({ question, dispatch, answer }) {
   return (
     <div className="options">
-      {question.options.map((option) => (
-        <button className="btn btn-option" key={option}>
+      {question.options.map((option, index) => (
+        <button
+          className={`btn btn-option ${index === answer ? "answer" : ""} ${
+            index === question.correctOption ? "correct" : "wrong"
+          }`}
+          key={option}
+          onClick={() => dispatch({ type: "newAnswer", payload: index })}
+        >
           {option}
         </button>
       ))}
